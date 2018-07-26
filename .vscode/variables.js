2e7c4a4832b40e7ef13e39e0ebd18c1fcbfbe7c4
The-lady-developer <manvisinghwal123@gmail.com>
2018-07-26 17:41:01 +0530

    javaScript

---
+++ .vscode/variables.js
@@ -0,0 +1,20 @@
+// Anything following double slashes is comment.
+
+// variable is a symbolic name for a value.
+//declared with the var keyword.
+var m;        //declared with a variable named m.
+
+//values can be assigned to variables with an = sign for example.
+m = 0;        //now the variable m has the value 0
+m             // => 0 : A variable evaluates to its value.
+
+
+//javaScript supports several types of values
+m = 1;       //numbers.
+m = 0.03;    // just number type for integers and reals.
+m = "hello world";  //strings of text in quotation marks.
+m = 'hello world';  //single quote mark also delimit strings.
+m = true;           //Boolean values.
+m = false;          //other Boolean values.
+m = null;           //Null is a special value that means *no value*.
+m = undefined;      //same like null.
