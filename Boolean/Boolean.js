867f1ba321539eaf620524ee517de759f7cf93b1
The-lady-developer <manvisinghwal123@gmail.com>
2018-08-01 22:36:22 +0530

    javaScript

---
+++ Boolean/Boolean.js
@@ -0,0 +1,13 @@
+//A boolean value represents truth or falsehood, on or off, yes or no. 
+//There are only two possible values of this type.
+
+a == 7 //comparision if the value of a is 7 then it's true otherwise it will return false.
+
+//if/else statement in JavaScript performs one action if a boolean value is true and another action if the value is false.
+
+
+if (a == 4)  
+b = b + 1;
+else  
+a = a + 1;
+
